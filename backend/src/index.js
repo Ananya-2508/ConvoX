56c9ecc3dbdf13d0e016a200091cdf49e6960036
Ananya-2508 <252586@kit.ac.in>
2026-07-22 23:03:33 +0530

    Add Clerk Express dependency

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./lib/db.js";
 
 import fs from "fs";
 import path from "path";
-
+const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
