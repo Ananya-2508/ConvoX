cea6235e5185b2a6e3bb866f2ae4f6d261cd0e16
Ananya-2508 <252586@kit.ac.in>
2026-07-27 14:55:51 +0530

    Fix Message schema field names

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,34 +1,29 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-    {
-        senderId:
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        recieverId:
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        test:
-        {
-            type: String,
-        },
-        image:
-        {
-            type: String,
-        },
-        video:
-        {
-            type: String,
-        },
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    { timestamps: true },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    video: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
-const Message = mongoose.model("message", messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 export default Message;
