b82e36e54ee2bb29ad536ed6a1b23d16fdc8c8b3
Ananya-2508 <252586@kit.ac.in>
2026-07-27 13:36:09 +0530

    fix: correct import statement for WallpaperContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { WallpaperProvider } from "./components/context/wallpaperContext";
+import { WallpaperContext } from "./components/context/WallpaperContext";
 import { ThemeProvider } from "./components/context/ThemeContext";
 import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
